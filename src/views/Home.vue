d3111608b4d66eba4231b0858c8227cc659cee3f
Hélen Andrade <50506811+HelenAndrade@users.noreply.github.com>
2024-06-12 11:04:35 -0300

    Adding usercard component and users.json (#1)
    Co-authored-by: HelenAndrade <helenaraujoandrade@hotail.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,26 @@
   <div class="app-container">
     <h1>Welcome to Home Page</h1>
     <p>This is the home page of our website.</p>
+
+    <UserCard v-for="user in users" :key="user.id" :user="user" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import UserCard from '@/components/UserCard.vue'
+import usersData from '../../public/users.json'
 
 export default defineComponent({
-  name: 'HomeView'
+  name: 'HomeView',
+  components: {
+    UserCard
+  },
+  data() {
+    return {
+      users: usersData
+    }
+  }
 })
 </script>
 
@@ -17,7 +29,10 @@ export default defineComponent({
 .app-container {
   display: flex;
   flex-direction: column;
-  width: 100vh;
+  align-items: center;
   background-color: rgb(34, 97, 97);
+  color: white;
+  height: fit-content;
+  width: 100vh;
 }
 </style>
