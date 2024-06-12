4c27fce8d83e825cefde84399565c28e38205a18
HelenAndrade <helenaraujoandrade@hotail.com>
2024-06-12 10:56:44 -0300

    Adding usercard component and users.json

---
+++ src/components/UserCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <a href="">
+    <div class="card">
+      <div class="card-content">
+        <h2>{{ user.name }}</h2>
+        <p>Email: {{ user.email }}</p>
+      </div>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  width: 80vh;
+  background-color: whitesmoke;
+  padding: 20px;
+  margin: 10px 5px 10px 5px;
+}
+
+.card-content {
+  color: #333;
+}
+</style>
