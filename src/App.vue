fca603fc0ab54c96fe129633f795a1d43732adae
HelenAndrade <helenaraujoandrade@hotail.com>
2024-06-13 22:11:43 -0300

    Adding Home View Service and renaming views

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <div class="wrapper">
         <nav>
           <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
+          <RouterLink to="/forms">Add new Contact</RouterLink>
         </nav>
       </div>
     </header>
---
+++ src/views/ContactProfile/contactprofile.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Contato</h1>
+</template>
+  
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ContactView'
+})
+</script>
+  
+<style scoped>
+  
+</style>
+  
