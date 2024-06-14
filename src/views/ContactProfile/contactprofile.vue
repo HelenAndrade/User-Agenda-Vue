c4f5d70e2893dae2853c77ed65141549ac89ec6d
Lindemir Hoffmann <lindemirhoffmann10@gmail.com>
2024-06-14 16:30:57 -0300

    create feature contactprofile

--- src/views/ContactProfile/contactprofile.vue
+++ src/views/ContactProfile/contactprofile.vue
@@ -1,16 +1,43 @@
 <template>
-    <h1>Contato</h1>
+    <div class="card" card>
+      <h1>Informação do Contato</h1>
+      <div class="card-info">
+      <p>ID: {{ contact.id }}</p>
+      <p>Name: {{ contact.name }}</p>
+      <p>Email: {{ contact.email }}</p>
+      <p>Number: {{ contact.number }}</p>
+    </div>
+    </div>
+  
+   
 </template>
   
 <script lang="ts">
-import { defineComponent } from 'vue'
 
-export default defineComponent({
-  name: 'ContactView'
-})
+export default{
+  name: 'ContactView',
+  data(){
+    return{
+      contact: {id: "1", name:"teste", email:"teste@gmail.com", number: '1020'}
+    }
+    
+  },created(){
+    console.log(this.contact)
+  }
+}
+
+  
 </script>
   
 <style scoped>
-  
+  .card-info {
+    width: 1000px;
+    background-color: whitesmoke;
+    padding: 20px;
+    font-size: 20px;
+  }
+  .card {
+    font-size: 30px;
+  }
 </style>
   
