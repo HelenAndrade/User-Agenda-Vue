c8db2be2c2402954e6a8920a159e9f5838ab1df1
Lindemir Hoffmann <108699223+LindemirHoffmann@users.noreply.github.com>
2024-06-19 16:34:16 -0300

    fix usercard (#13)

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,19 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import Card from 'primevue/card';
 import { setComponent } from './modules/components.module'
 
 const app = createApp(App)
 setComponent(app)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+})
+
+app.component('Card', Card);
 app.use(router)
 app.mount('#app')
