fd1f9eb4a5cd841e0c06d0f326fd88ea91562d8b
Carlos Henrique <110176211+chenriquedev@users.noreply.github.com>
2024-06-19 16:49:54 -0300

    feature/rest-post (#14)

--- src/views/Forms/forms.vue
+++ src/views/Forms/forms.vue
@@ -2,15 +2,41 @@
   <div class="app-container">
     <h1>Form New Contact</h1>
     <p>This is the form page of our website where you can create new contacts.</p>
+    <form action="#" method="post">
+      <input type="text" v-model="contact.name">
+      <input type="text" v-model="contact.email">
+      <input type="text" v-model="contact.number">
+      <button @click.prevent="saveContact(contact)">save contact</button>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { FormsService } from './forms.service'
+import { Contacts } from '@/model/contact.model';
 
 export default defineComponent({
-  name: 'FormsView'
+  name: 'FormsView',
+  data(){
+    return {
+      contact: new Contacts()
+      
+    }
+  },
+  computed:{
+    service(){
+      return new FormsService()
+    }
+
+  },
+  methods:{
+    saveContact(newContact: Contacts){
+      this.service.contact.pipe().subscribe({next: (response) => response})
+      this.service.postContact(newContact);
+      this.contact = {name: '', email: '', number: ''}
+    }
+  }
 })
 </script>
 
