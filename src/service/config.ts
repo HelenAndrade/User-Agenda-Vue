ae3b50eab6035694830ff952c0c3ce2d22de0a6f
HelenAndrade <helenaraujoandrade@hotmail.com>
2024-06-19 14:58:22 -0300

    Add Proxy Config

--- src/service/config.ts
+++ src/service/config.ts
@@ -1,33 +1,33 @@
-import axios, { AxiosError } from "axios";
-import type { AxiosRequestConfig, AxiosInstance } from "axios";
+import axios, { AxiosError } from 'axios'
+import type { AxiosRequestConfig, AxiosInstance } from 'axios'
 function apiConfig(baseUrl: string): AxiosRequestConfig {
   return {
-    baseURL: baseUrl,
-  };
+    baseURL: baseUrl
+  }
 }
 
 function initAxios(config: AxiosRequestConfig, token?: any): AxiosInstance {
-  const defineInstance = axios.create(config);
+  const defineInstance = axios.create(config)
   defineInstance.interceptors.request.use(
     (request: any) => {
-      return request;
+      return request
     },
     (error) => Promise.reject(error)
-  );
+  )
 
   defineInstance.interceptors.response.use(
     (response) => response,
     (error: AxiosError) => {
-      alert(error);
-      return Promise.reject(error);
+      alert(error)
+      return Promise.reject(error)
     }
-  );
+  )
 
-  return defineInstance;
+  return defineInstance
 }
 
-function api(baseURL= "https://6669f9352e964a6dfed7431b.mockapi.io/api/v1", token?: any) {
-  return initAxios(apiConfig(baseURL), token);
+function api(baseURL = '/api', token?: any) {
+  return initAxios(apiConfig(baseURL), token)
 }
 
-export default api;
+export default api
